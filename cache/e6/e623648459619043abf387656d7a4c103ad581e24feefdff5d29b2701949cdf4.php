791051aa83b606df3bc2293c857bb21138c9c4e8
ayopach3r <atpacheco@outlook.com>
2016-12-11 01:49:19 +0000

    Cambios del controlador

--- cache/e6/e623648459619043abf387656d7a4c103ad581e24feefdff5d29b2701949cdf4.php
+++ cache/e6/e623648459619043abf387656d7a4c103ad581e24feefdff5d29b2701949cdf4.php
@@ -32,6 +32,7 @@ class __TwigTemplate_b3bd7e29c77cb9065cf9061718520b1bec4b1c16a41012882cffc3d7a82
 \t<hr>
 \t<table class=\"table table-hover\">
 \t\t<tr>
+\t\t\t<th style=\"text-align:left;\">Id</th>
 \t\t\t<th style=\"text-align:left;\">Nombre</th>
 \t\t\t<th style=\"text-align:left;\">Fabricante</th>
 \t\t\t<th style=\"text-align:left;\">Origen</th>
@@ -40,29 +41,36 @@ class __TwigTemplate_b3bd7e29c77cb9065cf9061718520b1bec4b1c16a41012882cffc3d7a82
 \t\t\t<th></th>
 \t\t</tr>
 \t\t";
-        // line 16
+        // line 17
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["beers"]) ? $context["beers"] : null));
         foreach ($context['_seq'] as $context["_key"] => $context["r"]) {
-            // line 17
+            // line 18
             echo "\t\t<tr>
 \t\t\t<td>";
-            // line 18
-            echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "nombre"), "method"), "html", null, true);
-            echo "</td> <!-- '.' es igual a la '->' -->
-\t\t\t<td>";
             // line 19
-            echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "fabricante"), "method"), "html", null, true);
+            echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "id"), "method"), "html", null, true);
             echo "</td>
 \t\t\t<td>";
             // line 20
+            echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "nombre"), "method"), "html", null, true);
+            echo "</td> <!-- '.' es igual a la '->' -->
+\t\t\t<td>";
+            // line 21
+            echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "fabricante"), "method"), "html", null, true);
+            echo "</td>
+\t\t\t<td>";
+            // line 22
             echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "origen"), "method"), "html", null, true);
             echo "</td>
 \t\t\t<td>";
-            // line 21
+            // line 23
             echo twig_escape_filter($this->env, $this->getAttribute($context["r"], "__GET", array(0 => "cantidad"), "method"), "html", null, true);
             echo "</td>
-\t\t\t<td><a href=\"\">BORRAR</a></td> <!-- el : es igual a =>, y el [] a {} -->
+\t\t\t<td><a href=\"";
+            // line 24
+            echo twig_escape_filter($this->env, $this->env->getExtension('Slim\Views\TwigExtension')->pathFor("borrar", array("id" => $this->getAttribute($context["r"], "__GET", array(0 => "id"), "method"))), "html", null, true);
+            echo "\">BORRAR</a></td> <!-- el : es igual a =>, y el [] a {} -->
 \t\t\t<td><a href=\"\">EDITAR</a></td>  <!-- pathFor es igual a path_for -->
 \t\t\t
 \t\t</tr>
@@ -71,7 +79,7 @@ class __TwigTemplate_b3bd7e29c77cb9065cf9061718520b1bec4b1c16a41012882cffc3d7a82
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['r'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 27
+        // line 29
         echo "\t</table>
 ";
     }
@@ -88,7 +96,7 @@ class __TwigTemplate_b3bd7e29c77cb9065cf9061718520b1bec4b1c16a41012882cffc3d7a82
 
     public function getDebugInfo()
     {
-        return array (  75 => 27,  63 => 21,  59 => 20,  55 => 19,  51 => 18,  48 => 17,  44 => 16,  31 => 5,  28 => 4,  11 => 1,);
+        return array (  83 => 29,  72 => 24,  68 => 23,  64 => 22,  60 => 21,  56 => 20,  52 => 19,  49 => 18,  45 => 17,  31 => 5,  28 => 4,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -109,6 +117,7 @@ class __TwigTemplate_b3bd7e29c77cb9065cf9061718520b1bec4b1c16a41012882cffc3d7a82
 \t<hr>
 \t<table class=\"table table-hover\">
 \t\t<tr>
+\t\t\t<th style=\"text-align:left;\">Id</th>
 \t\t\t<th style=\"text-align:left;\">Nombre</th>
 \t\t\t<th style=\"text-align:left;\">Fabricante</th>
 \t\t\t<th style=\"text-align:left;\">Origen</th>
@@ -118,11 +127,12 @@ class __TwigTemplate_b3bd7e29c77cb9065cf9061718520b1bec4b1c16a41012882cffc3d7a82
 \t\t</tr>
 \t\t{% for r in beers %}
 \t\t<tr>
+\t\t\t<td>{{ r.__GET('id') }}</td>
 \t\t\t<td>{{ r.__GET('nombre') }}</td> <!-- '.' es igual a la '->' -->
 \t\t\t<td>{{ r.__GET('fabricante') }}</td>
 \t\t\t<td>{{ r.__GET('origen') }}</td>
 \t\t\t<td>{{ r.__GET('cantidad') }}</td>
-\t\t\t<td><a href=\"\">BORRAR</a></td> <!-- el : es igual a =>, y el [] a {} -->
+\t\t\t<td><a href=\"{{ path_for('borrar',{'id': r.__GET('id') }) }}\">BORRAR</a></td> <!-- el : es igual a =>, y el [] a {} -->
 \t\t\t<td><a href=\"\">EDITAR</a></td>  <!-- pathFor es igual a path_for -->
 \t\t\t
 \t\t</tr>
--- views/inicio.twig.php
+++ views/inicio.twig.php
@@ -6,6 +6,7 @@
 	<hr>
 	<table class="table table-hover">
 		<tr>
+			<th style="text-align:left;">Id</th>
 			<th style="text-align:left;">Nombre</th>
 			<th style="text-align:left;">Fabricante</th>
 			<th style="text-align:left;">Origen</th>
@@ -15,11 +16,12 @@
 		</tr>
 		{% for r in beers %}
 		<tr>
+			<td>{{ r.__GET('id') }}</td>
 			<td>{{ r.__GET('nombre') }}</td> <!-- '.' es igual a la '->' -->
 			<td>{{ r.__GET('fabricante') }}</td>
 			<td>{{ r.__GET('origen') }}</td>
 			<td>{{ r.__GET('cantidad') }}</td>
-			<td><a href="">BORRAR</a></td> <!-- el : es igual a =>, y el [] a {} -->
+			<td><a href="{{ path_for('borrar',{'id': r.__GET('id') }) }}">BORRAR</a></td> <!-- el : es igual a =>, y el [] a {} -->
 			<td><a href="">EDITAR</a></td>  <!-- pathFor es igual a path_for -->
 			
 		</tr>
